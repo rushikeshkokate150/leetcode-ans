c4c6ea8c55b9087e82ffeda4b1d34eaca41c1e37
Rushikesh Kokate <149380493+rushikeshkokate150@users.noreply.github.com>
2025-04-11 16:18:45 +0530

    Add files via upload

---
+++ missing number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public:
+        int findKthPositive(vector<int>& arr, int k) {
+            int n=arr.size();
+            int low=0;
+            // if(k<low) return k;
+            int high=n-1;
+            while(low<=high){
+                int mid=(low+high)/2;
+                int missing=arr[mid]-(mid+1);
+                if(missing<k) low=mid+1;
+                else high=mid-1;
+            }
+            return high+1+k;
+        }
+    };
