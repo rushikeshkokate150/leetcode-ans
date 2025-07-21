da67056c7438560fc2bddaf4a4d34591e4bdff33
Rushikesh Kokate <rushikeshkokate150@gmail.com>
2025-07-21 09:35:01 +0530

    Leetcode

---
+++ dfs.py
@@ -0,0 +1,20 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start)  # Process the node (e.g., print it)
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+    return visited
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+dfs(graph, 'A')
